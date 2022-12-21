92ac785a0aa3b2d98f04d806aa9ef7a6e6e3e963
Lucija <luc@luc.com>
2022-12-21 11:15:28 +0100

    Napravljena vježba

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+let num = 22;
+
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <div>
+        {num % 2 === 0 && <div>{num} je paran broj!</div>}
+        {num % 2 !== 0 && <div>{num} je neparan broj!</div>} 
+      </div>
+    </div>
   );
 }
 
